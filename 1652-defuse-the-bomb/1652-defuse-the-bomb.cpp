f698774f3bd48525cf4adaa32144af485cbd19bd
Ankit Kumar <ankits.kumar.cg@gmail.com>
2026-08-07 20:21:19 +0530

    Time: 0 ms (100%), Space: 11.7 MB (15.17%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
-        vector <int> ans;
-         int n = code.size();
-         if(k>0){
-         for(int i=0; i<n; i++){
-            int sum =0;
-            for(int j=1; j<=k; j++){
-                sum += code[(i+j)%n];
-            }
-                            ans.push_back(sum);
+        int n = code.size();
+        vector<int> ans;
 
-            }
-         }
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
 
-       else if(k<0){
-            for(int i=0; i<n; i++){
-                int sum=0;
-                for(int j=1; j<=-k; j++){
-                    sum += code[(i-j+n)%n];
-                    
-
+            if (k > 0) {
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % n];
                 }
-                ans.push_back(sum);
             }
-         }
-         else if(k==0){
-            for(int i=0; i<n; i++){
-                ans.push_back(0);
+            else if (k < 0) {
+                for (int j = -1; j >= k; j--) {
+                    sum += code[(i + j + n) % n];
+                }
             }
-         }
-         return ans;
+
+            ans.push_back(sum);
+        }
+
+        return ans;
     }
 };
