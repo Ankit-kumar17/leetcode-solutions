e42a536c231724ce3403634aa55605187788da97
Ankit Kumar <ankits.kumar.cg@gmail.com>
2026-09-09 10:10:34 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.05%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int original = x;
+        long long rev =  0;
+        
+        while(x>0){
+            int digit = x%10;
+            //  if(rev<= INT_MAX && rev>= INT_MIN){
+               
+            //  }
+            rev=rev*10+digit;
+           
+            x=x/10;
+        }
+        if(rev==original){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
